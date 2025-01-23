1b1d9485e0e6c9f5daa300103a164322c0c2698c
ale <delacorte19@gmail.com>
2025-01-23 09:55:54 +0100

    Se añade covidApi

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express")
 const PORT = 8000
 const app = express();
 const weatherController = require('./controllers/weatherController');
+const covidController = require('./controllers/covidController'); 
 
-app.use(weatherController);
+
+
+
+app.use(covidController);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
