f94a6cd72d0d54708d461fa7b98e03ee1bac5562
datadiego <juandiegomariscal@gmail.com>
2025-01-23 11:01:41 +0100

    fix: duplicados

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const nunjucks = require("nunjucks")
 const PORT = 8000
 const path = require('path');
 const bodyParser = require('body-parser');
-const PORT = 8000;
 const app = express();
 const weatherController = require('./controllers/weatherController');
 const covidController = require('./controllers/covidController'); 
 const coinsController = require('./controllers/coinsController');
 const moviesController = require('./controllers/moviesController');
 const translateController = require('./controllers/translateTextController');
-const moviesController = require('./controllers/moviesController');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public'))); // Aquí le indicamos a Express que los archivos estáticos están en la carpeta 'public'
