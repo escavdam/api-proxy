b45b0037bbfc5658d5ed074af967a32a00bc0155
datadiego <juandiegomariscal@gmail.com>
2025-01-23 09:54:38 +0100

    integramos nunjucks

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -2,11 +2,23 @@ const getWeather = require('../models/weatherModel');
 
 const router = require('express').Router();
 
-router.get('/weather', (req, res) => {
+router.get('/api/weather', (req, res) => {
     const city = req.query.city;
     getWeather(city)
         .then((response) => {
             res.send(response);
+        })
+        .catch((error) => {
+            res.status(500).send(error);
+        });
+});
+
+router.get('/weather', (req, res) => {
+    const city = req.query.city;
+    getWeather(city)
+        .then((response) => {
+            const weatherData = JSON.parse(response);
+            res.render('weather.html', { weather: weatherData });
         })
         .catch((error) => {
             res.status(500).send(error);
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express")
+const nunjucks = require("nunjucks")
 const PORT = 8000
 const app = express();
 const weatherController = require('./controllers/weatherController');
 
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.set('view engine', 'njk');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
 app.use(weatherController);
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
